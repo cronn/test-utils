1f851c3955785b0229c4dea1862c7ddcf2f65bdb
Radoslaw Postolowicz <radoslaw.postolowicz@cronn.de>
2023-01-16 15:17:23 +0100

    workaround sequence caching

--- spring-boot-3-tests/src/test/java/de/cronn/testutils/h2/app/SampleEntity.java
+++ spring-boot-3-tests/src/test/java/de/cronn/testutils/h2/app/SampleEntity.java
@@ -3,12 +3,14 @@ package de.cronn.testutils.h2.app;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 
 @Entity
 public class SampleEntity {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(generator = "my_explicit_generator")
+	@SequenceGenerator(name = "my_explicit_generator", allocationSize = 1, sequenceName = "SAMPLE_ENTITY_SEQ")
 	private Integer id;
 
 	public Integer getId() {
