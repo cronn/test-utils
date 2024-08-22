443095d60ce287da6a4c0519c7533c56546dafcd
Benedikt Waldvogel <benedikt.waldvogel@cronn.de>
2024-08-22 10:42:35 +0200

    Introduce ExecutorServiceExtensionTest

---
+++ src/test/java/de/cronn/testutils/ExecutorServiceExtensionTest.java
@@ -0,0 +1,34 @@
+package de.cronn.testutils;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import java.util.List;
+import java.util.concurrent.Future;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(ThreadLeakCheck.class)
+class ExecutorServiceExtensionTest {
+
+    @RegisterExtension
+    ExecutorServiceExtension executorServiceExtension = new ExecutorServiceExtension(10_000);
+
+    @Test
+    void testHappyCase() throws Exception {
+        executorServiceExtension.submit(() -> "one");
+        executorServiceExtension.submit(() -> "two");
+        executorServiceExtension.submit(() -> "three");
+
+        executorServiceExtension.awaitAllFutures();
+
+        List<Future<?>> futures = executorServiceExtension.getFutures();
+        assertThat(futures).hasSize(3);
+        assertThat(futures)
+                .map(Future::get)
+                .map(Object::toString)
+                .containsExactlyInAnyOrder("one", "two", "three");
+    }
+
+}
